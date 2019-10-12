306ae972628e098f3f414f1c3a30804be4b71f26
Thomas Beduneau <thomas@switch-company.com>
2019-10-12 10:56:11 +0200

    Add Danger module to force PR description

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,9 @@
-import { danger, markdown } from 'danger'
+import { danger, warn } from 'danger'
 
-const modifiedMD = danger.git.modified_files.join("\n- ")
-markdown("Changed Files in this PR: \n- " + modifiedMD)
+if (danger.github.pr.body.length < 5) {
+  warn(
+    `La description d'une pull request permet de comprendre le contexte du \
+     code produit et facilite la revue de code. Prenez le temps de la \
+     détailler davantage`
+  )
+}
