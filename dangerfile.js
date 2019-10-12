ae0c6b430dfe7c2205a5b7fee07defaa617f4574
Thomas Beduneau <thomas@switch-company.com>
2019-10-12 11:02:46 +0200

    replace warn with fail

--- dangerfile.js
+++ dangerfile.js
@@ -1,7 +1,7 @@
-import { danger, warn } from 'danger'
+import { danger, fail } from 'danger'
 
 if (danger.github.pr.body.length < 5) {
-  warn(
+  fail(
     `La description d'une pull request permet de comprendre le contexte du \
      code produit et facilite la revue de code. Prenez le temps de la \
      détailler davantage`
